466b4c8e852bf6a50cf8280f804d5fb5380c1e83
Pedro Henrique <pedro.henrique.torres@hotmail.com>
2022-10-14 09:48:32 -0300

    feat: Create page SignIn

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { SignIn } from './SignIn';
+
+export default {
+  title: 'Pages/Sign In',
+  component: SignIn,
+} as Meta;
+
+export const Default: StoryObj = {};
