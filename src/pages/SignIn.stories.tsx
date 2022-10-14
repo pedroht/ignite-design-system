2c86b423902fb7d6c357ee0beb465b99b35271b2
Pedro Henrique <pedro.henrique.torres@hotmail.com>
2022-10-14 10:09:31 -0300

    test: Add first test

--- src/pages/SignIn.stories.tsx
+++ src/pages/SignIn.stories.tsx
@@ -1,4 +1,6 @@
+import { expect } from '@storybook/jest';
 import { Meta, StoryObj } from '@storybook/react';
+import { userEvent, waitFor, within } from '@storybook/testing-library';
 import { SignIn } from './SignIn';
 
 export default {
@@ -6,4 +8,20 @@ export default {
   component: SignIn,
 } as Meta;
 
-export const Default: StoryObj = {};
+export const Default: StoryObj = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    userEvent.type(
+      canvas.getByPlaceholderText('Digite o seu e-mail'),
+      'pedro.henrique.torres@hotmail.com'
+    );
+    userEvent.type(canvas.getByPlaceholderText('*********'), '1234567890');
+
+    userEvent.click(canvas.getByRole('button'));
+
+    await waitFor(() => {
+      return expect(canvas.getByText('Login realizado!')).toBeInTheDocument();
+    });
+  },
+};
